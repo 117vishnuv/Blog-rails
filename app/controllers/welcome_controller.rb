ec4608830dd3e3a526b36fe5f30a4d90b9678d07
vishnu_v <117vishnuv@gmail.com>
2020-12-21 14:11:11 +0530

    branch third final commit , (category, trix styling,)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,14 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
   def index
-    @articles = Article.all
+    @categories = Category.all
+        cate = params[:cate]
+        if !cate.nil?
+          @articles = Article.where(:category_id => cate )
+        elsif cate=="All"
+          @articles = Article.all   
+        else
+          @articles = Article.all
+        end 
   end
 end
